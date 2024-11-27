7095500caf7f439e6d1eee70a12635ded3a6f6a9
boosoyz <boosoyz@gmail.com>
2024-11-27 20:47:25 +0800

    Update mtg.sh

--- mtg.sh
+++ mtg.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# 获取当前脚本的绝对路径，并设置 mtg 目录和日志路径
+# 获取当前脚本的绝对路径，并设置 mtg 目录
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 MTG_DIR="${BASE_DIR}/mtg"
-KEEP_ALIVE_LOG="${MTG_DIR}/keep_alive.log"
 
 # 创建 mtg 目录
 mkdir -p "${MTG_DIR}"
 cd "${MTG_DIR}" || { echo "无法进入目录 ${MTG_DIR}"; exit 1; }
 
 # 下载 mtg 可执行文件并赋予执行权限
-echo "正在下载 mtg..."
-curl -LO https://rain3.serv00.net/serv00-app/mtproto/mtg > /dev/null 2>&1
+curl -LO https://raw.githubusercontent.com/boosoyz/mtproto/main/mtg > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "下载失败，请检查网络连接。"
     exit 1
@@ -35,12 +33,6 @@ fi
 # 让用户手动输入端口
 read -p "请输入 mtg 使用的端口号: " port
 
-# 检查输入的端口是否有效
-if [[ ! "$port" =~ ^[0-9]+$ || "$port" -lt 1024 || "$port" -gt 65535 ]]; then
-    echo "无效的端口号。请输入一个有效的端口（1024-65535）。"
-    exit 1
-fi
-
 mtpport="$port"
 echo "使用的端口为：$mtpport"
 
@@ -58,7 +50,7 @@ nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:${mtpport} ${secret} -c 
 
 # 检查 mtg 是否启动成功
 sleep 3
-if pgrep -f "./mtg" > /dev/null; then
+if pgrep -x "mtg" > /dev/null; then
     mtproto="https://t.me/proxy?server=${host}&port=${mtpport}&secret=${secret}"
     echo "生成的 mtproto 链接：$mtproto"
     echo "启动成功"
@@ -72,12 +64,9 @@ cat > "${MTG_DIR}/keep_alive.sh" <<EOL
 #!/bin/bash
 
 # 检查TCP端口是否有进程在监听
-if ! netstat -tuln | grep -q "0.0.0.0:${mtpport}"; then
-    echo "[\$(date)] 端口 ${mtpport} 未监听，尝试重启 mtg。" > /dev/null 2>&1
+if ! sockstat -4 -l | grep -q "0.0.0.0:${mtpport}"; then
     cd "${MTG_DIR}"
-    TMPDIR="${MTG_DIR}/" nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:${mtpport} ${secret} -c 8192 --prefer-ip="prefer-ipv6" > /dev/null 2>&1 &
-else
-    echo "[\$(date)] mtg 正在运行。" > /dev/null 2>&1
+    TMPDIR="${MTG_DIR}/" nohup ./mtg simple-run -n 1.1.1.1 -t 30s -a 1MB 0.0.0.0:${mtpport} ${secret} -c 8192 > /dev/null 2>&1 &
 fi
 EOL
 
